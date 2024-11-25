23d825dad6a3996960205b79ef0e1ed3a6e7851b
Harri Kalikka <harri.kalikka@gmail.com>
2024-11-25 23:10:03 +0200

    fixes4

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import express, { statica } from "express";
-const app = express();
+import express, { statica } from 'express'
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.use(statica("dist"));
+app.use(statica("dist"))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
-});
+  console.log(`server started on port ${PORT}`)
+})
