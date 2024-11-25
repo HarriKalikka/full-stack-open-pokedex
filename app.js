2cea59761831709376960a18b7c2be3afd967b2c
Harri Kalikka <harri.kalikka@gmail.com>
2024-11-25 23:22:50 +0200

    fixes7

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var process = app.NodeJS.process
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(statica("dist"))
+app.use(statica('dist'))
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
+  //console.log(`server started on port ${PORT}`)
 })
